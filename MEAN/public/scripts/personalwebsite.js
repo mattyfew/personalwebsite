c66027c280352a46579498c6889ef8bc66284d08
Matt Fewer <mattfewer@hotmail.com>
2015-11-26 02:19:13 -0500

    added wow.js, mobile not working

--- MEAN/public/scripts/personalwebsite.js
+++ MEAN/public/scripts/personalwebsite.js
@@ -1,11 +1,41 @@
-$(document).ready(function () {
+$(document).ready(function() {
 	
-		$('#fullpage').fullpage({
-			navigation: true,
-			navigationPosition: 'left',
-			navigationTooltips: ['Home', 'About Me', 'Skills', 'Portfolio', 'Contact', 'Hire Me'],
-			slidesNavigation: true,
-			slidesNavPosition: 'bottom'
-		});
+	wow = new WOW(
+		{
+			boxClass:     'wow',      // default
+			animateClass: 'animated', // default
+			offset:       0,          // default
+			mobile:       false,      
+			live:         true        // default
+		}
+	)
+	wow.init();
 
-})
+	$('#fullpage').fullpage({
+		navigation: true,
+		navigationPosition: 'left',
+		navigationTooltips: ['Home', 'About Me', 'Skills', 'Portfolio', 'Contact', 'Hire Me'],
+		resize: false,
+		scrollBar: true,
+		scrollOverflow: false,
+
+		//RESPONSIVE
+		responsiveWidth: 800,
+		afterResize: function () {
+			if ($(window).width() < 800) {
+				$.fn.fullpage.setAllowScrolling(false);
+				$.fn.fullpage.setKeyboardScrolling(false);
+				
+//				$('#fullpage').fullpage({scrollOverflow:true});
+				var verticalNav = document.getElementById("fp-nav");
+				$(verticalNav).hide();
+			}
+		}
+	});
+	
+	if($(window).width() <= 719){
+
+			$(".wow").removeClass("wow");
+
+	}
+});
