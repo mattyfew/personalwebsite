60e250340a6ce509b8cb3b1817ed98fe66e9b0bc
Matt Fewer <mattfewer@hotmail.com>
2015-12-06 00:23:11 -0500

    changed port, added debug, made image smaller

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 var express = require('express');
 var app = express();
+var debug = require('debug')('cli-app');
 //var ejs = require('ejs');
 var bodyParser = require('body-parser');
-var urlencodedBodyParser = bodyParser.urlencoded({extended: false});
+var urlencodedBodyParser = bodyParser.urlencoded({
+	extended: false
+});
 //var sqlite3 = require('sqlite3').verbose();
 //var db = new sqlite3.database('website.db');
 //var methodOverride = require('method-override');
+
+app.set('port', process.env.PORT || 3000);
 
 app.use(urlencodedBodyParser);
 app.set('view_engine', 'ejs');
 //app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
-app.get('/about', function(req,res){
-	res.render('about.html.ejs')
-})
-
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
 	res.render('index.html.ejs')
-	
 })
 
-app.listen(3000, function(err){
-	console.log('listening on port 3000');
-})
+var server = app.listen(app.get('port'), function() {
+	console.log('Listening on port ' + server.address().port)
+	debug('Express server listening on port ' +  server.address().port);
+});
+
+//var listener = app.listen(8888, function(){
+//	console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+//});
+
+//app.listen(80, function(err){
+//	console.log('listening on port 3000');
+//})
